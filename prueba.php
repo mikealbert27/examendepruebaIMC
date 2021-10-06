3b45da95ebb623c96d6d12b40b7f37ee4edf3c3c
Miguel Alberto Padilla Villegas <miguelpadila191@gmail.com>
2021-10-05 18:41:16 -0700

    Codigo completo

--- prueba.php
+++ prueba.php
@@ -9,16 +9,16 @@ class Persona{
         $imc=($peso)/($estatura*$estatura);
         
         if($imc<18.5){
-            echo ($nombre .' tu tienes bajo peso');
+            echo ($nombre . ' tienes una altura de: '. $estatura . ', un peso de ' .$peso .'kg' . ' tu imc es: Peso saludable');
 
         }elseif($imc>=18.5 && $imc<=24.9){
-          echo $nombre .' tu peso es normal';
+          echo $nombre . ' tienes una altura de: ' . $estatura . ', un peso de ' .$peso .'kg' .' tu imc es: Peso normal';
  
         }elseif($imc>=25 && $imc<=29.9){
-          echo $nombre .' tu tienes sobrepeso';
+          echo $nombre .' tienes una altura de: '. $estatura . ', un peso de ' .$peso.'kg' .'tu imc es: Sobrepeso';
  
         }elseif($imc>=30){
-          echo $nombre . 'tienes obesidad';
+          echo $nombre . ' tienes una altura de: ' . $estatura . ', un peso de ' .$peso. 'kg'.' tu imc es: Obesidad';
         }
         
     }
--- resultadoIMC.php
+++ resultadoIMC.php
@@ -3,4 +3,4 @@ require_once 'prueba.php';
 
 $resultadoimc = new Persona;
 
-$resultadoimc->calcularIMC('Miguel', 1.70, 60.00);
+$resultadoimc->calcularIMC('Miguel', 1.70, 95.00);
